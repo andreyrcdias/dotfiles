a27c991fe54b15555f69f3b5a7566c3d8411b361
andreyrcdias <andreycadimaa@gmail.com>
2023-01-01 10:09:50 -0300

    fix nvim text yank

--- .config/nvim/lua/arcd/init.lua
+++ .config/nvim/lua/arcd/init.lua
@@ -1,3 +1,26 @@
 require("arcd.remap")
 require("arcd.set")
 
+local augroup = vim.api.nvim_create_augroup
+local arcd = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = arcd,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
