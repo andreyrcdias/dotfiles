ec69e6750ece72fc8d3a4983e3a0883b13e02962
andreyrcdias <andreycadimaa@gmail.com>
2023-01-01 21:46:46 -0300

    nvm quit remap

--- .config/nvim/lua/arcd/remap.lua
+++ .config/nvim/lua/arcd/remap.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>w", vim.cmd.write)
+vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
