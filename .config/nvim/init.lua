02960b08318d676c4a02f76c495595a9bf01dada
andreyrcdias <andreycadimaa@gmail.com>
2023-01-21 12:54:30 -0300

    ide: cleanup nvim cfgs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,2 @@
 require("arcd")
 
-require("vscode").setup({
-    transparent = true,
-    disable_nvimtree_bg = true,
-})
-
-
-require("gitsigns").setup()
-
--- require("lualine").setup{
---     options = {
---        icons_enabled = false,
---    }
---}
-
-require("indent_blankline").setup()
-
