d573501e96f870c66fbdd365c9d7cab4f7f5e42e
andreyrcdias <andreycadimaa@gmail.com>
2023-04-05 14:24:55 -0300

    fix: clean up

--- .config/nvim/lua/arcd/remap.lua
+++ .config/nvim/lua/arcd/remap.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
 vim.keymap.set("n", "<leader>w", vim.cmd.write)
 vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
 
@@ -21,12 +20,9 @@ vim.keymap.set("n", "<leader>svwm", function()
 end)
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
-
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
