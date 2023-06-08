41a44da8ff779ce5fe6888398730537417453395
andreyrcdias <andreycadimaa@gmail.com>
2023-06-08 03:41:44 -0300

    macos: settings

--- macos/install.sh
+++ macos/install.sh
@@ -13,3 +13,8 @@ defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock magnification -float 1.0
 defaults write com.apple.dock mineffect -string "genie"
+
+# Control Center
+defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -bool true
+defaults write com.apple.controlcenter "NSStatusItem Visible Battery" -bool true
+
