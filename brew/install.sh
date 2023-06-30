da236199d648e4abd26851673b6b9d483b6d1402
andreyrcdias <andreycadimaa@gmail.com>
2023-06-30 11:59:18 -0300

    code cleanup

--- brew/install.sh
+++ brew/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-if test ! "$(uname)" = "Darwin"
-  then
+if test ! "$(uname)" = "Darwin"; then
   exit 0
 fi
 
--- macos/install.sh
+++ macos/install.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-if test ! "$(uname)" = "Darwin"
-  then
+if test ! "$(uname)" = "Darwin"; then
   exit 0
 fi
 
-# Finder
 defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder ShowPathbar -bool true
 
-# Dock
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock magnification -float 1.0
 defaults write com.apple.dock mineffect -string "genie"
 
-# Control Center
 defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -bool true
 defaults write com.apple.controlcenter "NSStatusItem Visible Battery" -bool true
 
