dc840644096bc5041da661381660d79a3f0fc909
andreyrcdias <andreycadimaa@gmail.com>
2023-12-25 20:28:29 -0300

    update macos leaves and configs

--- macos/install.sh
+++ macos/install.sh
@@ -4,6 +4,7 @@ if test ! "$(uname)" = "Darwin"; then
   exit 0
 fi
 
+
 defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder ShowPathbar -bool true
 
@@ -13,4 +14,11 @@ defaults write com.apple.dock mineffect -string "genie"
 
 defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -bool true
 defaults write com.apple.controlcenter "NSStatusItem Visible Battery" -bool true
+
+# Vim + VScode
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+defaults write com.vscodium ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeExploration ApplePressAndHoldEnabled -bool false
+defaults delete -g ApplePressAndHoldEnabled
 
