b4c87190923da5b5992d032f48ed766f5fe6dbf2
andreyrcdias <andreycadimaa@gmail.com>
2024-03-10 15:47:38 -0300

    sync update

--- nvim/init.lua
+++ nvim/init.lua
@@ -729,9 +729,8 @@ require("lazy").setup({
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
 		"blazkowolf/gruber-darker.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
+		init = function()
 			-- Load the colorscheme here
 			vim.cmd.colorscheme("gruber-darker")
 
