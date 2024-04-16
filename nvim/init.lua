3548d3eccb9d4e8abdf30e5586e8f2094a24dcc7
Andrey <andreyrcdias@gmail.com>
2024-04-16 17:04:25 -0300

    feat(nvim): add tree-siter go grammar

--- nvim/init.lua
+++ nvim/init.lua
@@ -792,7 +792,7 @@ require("lazy").setup({
 
 			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "rust", "python" },
+				ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "rust", "python", "go" },
 				-- Autoinstall languages that are not installed
 				auto_install = true,
 				highlight = { enable = true },
