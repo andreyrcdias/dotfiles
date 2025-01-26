bf47aa5b2b97af2f816934b0568e3f721824a8c4
Andrey <andreyrcdias@gmail.com>
2025-01-26 08:17:53 -0300

    vim: config gruber darker colorscheme

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+mkdir -p ~/.vim/colors
+
+wget https://raw.githubusercontent.com/drsooch/gruber-darker-vim/refs/heads/master/colors/GruberDarker.vim -O ~/.vim/colors/gruber-darker.vim
+
 "$S" "$HEREP/vimrc" "$OUT/.vimrc"
