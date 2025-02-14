23c9814423e607ad5d60d870065e6bdb343cf352
Andrey <andreyrcdias@gmail.com>
2025-02-14 08:19:20 -0300

    tmux: added tpm installation

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# https://github.com/tmux-plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 "$S" "$HEREP/tmux.conf" "$OUT/.tmux.conf"
 
-# https://github.com/tmux-plugins/tpm
